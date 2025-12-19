b4e23c7079cfe0c21f57bd6a99695a7e74127d70
ayushk107 <ayushk18022002@gmail.com>
2025-12-19 13:06:25 +0530

    Fix mobile responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import dynamic from "next/dynamic";
-import dynamic from "next/dynamic";
-
 const WelcomeSection = dynamic(() => import("./components/WelcomeSection"));
 const GridLayout = dynamic(() => import("./components/GridLayout"));
 
